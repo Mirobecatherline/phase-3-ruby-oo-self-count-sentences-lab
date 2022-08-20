31f73107a26fe5603c955c91e9fa6d7391d31650
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-08-20 12:29:12 +0300

    commit tree 4b940cd8f0f138a1e2b7814594f4c47bfbdcfabe

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,17 +4,29 @@ class String
 
   def sentence?
     
+    if self.end_with?(".") 
+    return true
+    else false
+  end
+
   end
 
   def question?
-
+    if self.end_with?("?") 
+      return true
+      else false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") 
+      return true
+      else false
+    end
   end
 
   def count_sentences
-
+   self.split("/(\?|\.|!)/").count
+  
   end
 end
